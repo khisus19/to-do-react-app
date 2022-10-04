4b162ae4ba0060e6ee4ebe243ec3bf350d9c4989
Jesús Álvarez <khisus19@gmail.com>
2022-10-04 12:57:54 -0500

    Implement the react-icons and cleaning the svgs

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import "../styles/TodoItem.css"
 import HoverImage from "react-hover-image";
+import { IoTrashBin, IoCheckmarkCircleSharp } from "react-icons/io5"
 
 export default function TodoItem(props) {
 
   return (
     <li className={props.completed ? "todo-item completed" : "todo-item"}
       onClick={() => props.handle_complete(props.id)}>
-      <img 
-      src={props.completed ? "./check-icon.svg" : "./check-icon-dark.svg"} 
-      className="todo-icon" />
+      <IoCheckmarkCircleSharp 
+        className={props.completed ? "todo-icon checked" : "todo-icon"} />
       <p className="todo-text">{props.text}</p>
-      <HoverImage 
+      <IoTrashBin 
         className="todo-icon delete"
-        src="./garbage-bin.svg"
-        hoverSrc="./garbage-bin-red.svg"
         onClick={() => props.handle_delete(props.id)} />
     </li>
   )
