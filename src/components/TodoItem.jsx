4aa8d678e5164a6c6cdd2fa793ecaa7a351f7406
Jesús Álvarez <khisus19@gmail.com>
2022-10-04 18:50:22 -0500

    Changing color palette

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import "../styles/TodoItem.css"
-import HoverImage from "react-hover-image";
-import { IoTrashBin, IoCheckmarkCircleSharp } from "react-icons/io5"
+import { IoTrashBin, IoCheckmarkCircleSharp, IoEllipseOutline } from "react-icons/io5"
 
 export default function TodoItem(props) {
 
   return (
     <li className={props.completed ? "todo-item completed" : "todo-item"}
       onClick={() => props.handle_complete(props.id)}>
-      <IoCheckmarkCircleSharp 
-        className={props.completed ? "todo-icon checked" : "todo-icon"} />
+      {props.completed ? 
+        <IoCheckmarkCircleSharp 
+          className="todo-icon checked" /> :
+        <IoEllipseOutline
+          className="todo-icon" />
+      }
       <p className="todo-text">{props.text}</p>
       <IoTrashBin 
         className="todo-icon delete"
--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import "../styles/TodoSearch.css"
 
 export default function TodoSearch(props) {
-
-
   return (
-    <input placeholder="Cebolla" onChange={props.search}/>
+    <input placeholder="Search" onChange={props.search}/>
   )
 }
